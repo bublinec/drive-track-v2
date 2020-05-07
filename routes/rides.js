6af421299d3326d75c7c9c327d58705ca6934360
Juraj-Bublinec <jurajbublinec@gmail.com>
2020-05-07 08:46:17 +0100

    Fix date sorting

--- routes/rides.js
+++ routes/rides.js
@@ -16,6 +16,8 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     var input_arr = req.body.date_input.split(".");
     date_str= input_arr[1] + "/" + input_arr[0] + "/" + input_arr[2];
     new_ride.date = new Date(date_str);
+    console.log(new_ride.date);
+    
     // get round trip checkbox
     if(req.body.round_trip == "on"){
         new_ride.round_trip = true;
